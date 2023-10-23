43e0cd4210d57ae6342322209b6b6e083272243d
yeeecheng <yichengliao1003@gmail.com>
2023-10-23 22:04:50 +0800

    delete test.pt

--- test.py
+++
@@ -1,5 +0,0 @@
-from agents.intelligent_agent.intelligent_agent import intelligent_agent_test
-from pathlib import Path
-if __name__ == '__main__':
-    a = intelligent_agent_test(server_url = "http://localhost:8001" , openai_token=Path("doc/secret/openai.key") )
-    while a.checker != False: pass
