83e5f05b8b02383edbd8d0cff3d980d52dd401ff
Pinpunyu <pe25617259@gmail.com>
2023-11-04 23:57:59 +0800

    add summary class load prompt & open-ai send function

--- agents/summary.py
+++ agents/summary.py
@@ -27,6 +27,82 @@ class summary():
         self.room_name = room_name
         self.logger : logging.Logger = logger
         self.prompt_dir = Path(prompt_dir)
-        # self.__load_prompt_and_example__(self.prompt_dir)
+        self.__load_prompt_and_example__(self.prompt_dir)
 
+    def __load_prompt_and_example__(self , prompt_dir):
+        """load prompt json to dict"""
+        self.logger.debug("load common json")
+        with open(prompt_dir / "common_prompt.json" , encoding="utf-8") as json_file: self.prompt_template = json.load(json_file)
+        with open(prompt_dir / "common_example.json" , encoding="utf-8") as json_file: self.example = json.load(json_file)
+
+        for key , prompt_li in self.prompt_template.items():
+            self.prompt_template[key] = '\n'.join(prompt_li)
+        for key , prompt_li in self.example.items():
+            self.example[key] = '\n'.join(prompt_li)
+
+    def __openai_send__(self , prompt):
+        """openai api send prompt , can override this."""
+        response = openai.ChatCompletion.create(
+            engine = self.engine,
+            messages = [
+                {"role":"system","content":"You are an AI assistant that helps people find information."},
+                {"role":"user","content":prompt}
+            ],
+            temperature=0.7,
+            max_tokens=800,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None)
+        
+        self.token_used += response["usage"]["total_tokens"]
+        
+        if response['choices'][0]["finish_reason"] == "content_filter":
+            self.logger.debug("Block By Openai")
+            return None
+
+        return response['choices'][0]['message']['content']
     
+    def __process_LLM_output__(self , prompt , keyword_list , sample_output , max_fail_cnt = -1):
+        """
+        communication with LLM , repeat {max_fail_cnt} util find the {keyword_list} in LLM response .
+        return the {keyword_list} dict , if fail get {keyword_list} in LLM response , return {sample_output}.
+        """
+        # max_fail_cnt = self.max_fail_cnt
+        success_get_keyword = False
+        fail_idx = 0
+
+        self.logger.debug(f"LLM keyword : {keyword_list}")
+        info = {}
+
+        while not success_get_keyword and fail_idx < self.max_fail_cnt:
+
+            self.logger.debug(f"start {fail_idx} prompt")
+            info = {}
+            result = self.__openai_send__(prompt)
+
+            # result block by openai
+            if result == None:
+                fail_idx+=1
+                continue
+            
+            
+            splited_result = result.split('\n')
+            keyword_name = ""
+            for line in splited_result:
+                # get keyword like [XXX]
+                keyword = re.search('\[(.*)\]', line)
+                if keyword != None and keyword.group(1) in self.chinese_to_english.keys():
+                    keyword_name = self.chinese_to_english[keyword.group(1)]
+                    info[keyword_name] = ""
+                elif keyword_name != "":
+                    info[keyword_name] += line + "\n"
+
+            if all(_ in info.keys() for _ in keyword_list): success_get_keyword = True
+            else : fail_idx+=1
+        
+        self.logger.debug(f"LLM output : {info}")
+
+        if fail_idx >= max_fail_cnt: info = sample_output
+
+        return info
