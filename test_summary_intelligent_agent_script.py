c9f08bfd4693122ace9da6aee80e493c51ee1716
yeeecheng <yichengliao1003@gmail.com>
2023-11-24 02:43:13 +0800

    test summary intelligent agent script

---
+++ test_summary_intelligent_agent_script.py
@@ -0,0 +1,4 @@
+from agents.intelligent_agent.intelligent_agent import summary_intelligent_agent_script
+
+if __name__ == "__main__":
+    summary_intelligent_agent_script(api_json= "./doc/secret/openai.key", game_info_path= "./doc/game_info/game3/11_19_23_09_agent0.jsonl")
---
+++ test_summary_script.py
@@ -0,0 +1,4 @@
+from agents.script_agent import summary_script_agent
+
+if __name__ == "__main__":
+    summary_script_agent(api_json= "./doc/secret/openai.key", game_info_path= "./doc/game_info/game2/11_19_23_13_agent6.jsonl")
