6bd683acd9b7665e8897bb39b9283aaa949120aa
yeeecheng <yichengliao1003@gmail.com>
2023-12-01 02:23:59 +0800

    modify mapping role

--- cal_human_acc.py
+++ cal_human_acc.py
@@ -6,10 +6,10 @@ raw_data = pd.read_csv("./human_sheet.csv")
 record_x_by_agent = {num:[] for num in range(8)}
 
 guess_map_score = {
-    "預言家" : ["好人", "預言家"],
+    "預言家" : ["好人","神職","預言家"],
     "狼人" : ["狼人"],
-    "女巫" : ["好人", "女巫"],
-    "獵人" : ["好人", "獵人"],
+    "女巫" : ["好人","神職", "女巫"],
+    "獵人" : ["好人","神職", "獵人"],
     "平民" : ["好人", "平民"],
 }
 
@@ -38,7 +38,15 @@ def save_scrip():
                 if guess in guess_map_score[ans]:
                     # good 
                     if len(guess_map_score[ans]) == 2:
-                        score += 0.5 if guess == "好人" else 1
+                        if guess == "好人":
+                            score += 0.5
+                        elif guess == "平民":
+                            score += 1
+                    elif len(guess_map_score[ans]) == 3:
+                        if guess in ["好人", "神職"]:
+                            score += 0.5
+                        elif guess in ["預言家", "女巫", "獵人"]:
+                            score += 1
                     # bad
                     else:
                         score += 1 if guess == "狼人" else 0
@@ -58,6 +66,9 @@ def save_scrip():
                 with open(save_file , 'a+' , encoding='utf-8') as f :
                     json.dump(result_dic , f , ensure_ascii=False)
                     f.write('\n')
+
+
+save_scrip()
 
 with open(f"./human_script.jsonl" , encoding="utf-8") as json_file: 
     raw_data = [json.loads(line) for line in json_file.readlines()]
